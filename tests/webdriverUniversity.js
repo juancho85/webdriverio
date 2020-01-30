406f16497a9318b9546facad41812da5a18f5215
Juan David Sánchez <juancho85@gmail.com>
2020-01-30 21:46:01 +0100

    Enable sync mode and refactor tests accordingly

--- tests/webdriverUniversity.js
+++ tests/webdriverUniversity.js
@@ -1,25 +1,21 @@
 describe('Verify whether webdriveruniversity links on homepage work correcly', function() {
     it('check that the contact us button opens the contact us page', function(done) {
-        return browser
-            .setViewportSize({
-                width: 1200,
-                height: 800
-            })
-            .url('http://www.webdriveruniversity.com/')
-            .getTitle().then(function (title) {
-            console.log('Title is: ' + title); // Log navigation through the home page
-        })
-            .click('#contact-us')
-            .pause(3000); // Pause to see what is going on (value in milliseconds)
+        browser.setViewportSize({
+            width: 1200,
+            height: 800
+        });
+        browser.url('http://www.webdriveruniversity.com/')
+        var title = browser.getTitle();
+        console.log('Title is: ' + title); // Log navigation through the home page
+        browser.click('#contact-us');
+        browser.pause(3000); // Pause to see what is going on (value in milliseconds)
     });
 
     it('check that login button opens the login portal page', function(done) {
-        return browser
-            .url('http://www.webdriveruniversity.com/')
-            .click('#login-portal')
-            .getTitle().then(function (title) {
-            console.log('Title is: ' + title);
-        })
-            .pause(3000);
+        browser.url('http://www.webdriveruniversity.com/');
+        browser.click('#login-portal');
+        var title = browser.getTitle();
+        console.log('Title is: ' + title);
+        browser.pause(3000);
     });
 });
