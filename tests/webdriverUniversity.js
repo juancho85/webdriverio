db50aa41a7ce9b55c5efc7b3ac2d6b72e511a7a2
Juan David Sánchez <juancho85@gmail.com>
2020-01-30 22:08:10 +0100

    Base URL and environments

--- tests/webdriverUniversity.js
+++ tests/webdriverUniversity.js
@@ -4,7 +4,7 @@ describe('Verify whether webdriveruniversity links on homepage work correcly', f
             width: 1200,
             height: 800
         });
-        browser.url('http://www.webdriveruniversity.com/')
+        browser.url('/')
         var title = browser.getTitle();
         console.log('Title is: ' + title); // Log navigation through the home page
         browser.click('#contact-us');
@@ -12,7 +12,7 @@ describe('Verify whether webdriveruniversity links on homepage work correcly', f
     });
 
     it('check that login button opens the login portal page', function(done) {
-        browser.url('http://www.webdriveruniversity.com/');
+        browser.url('/');
         browser.click('#login-portal');
         var title = browser.getTitle();
         console.log('Title is: ' + title);
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,11 @@
+var baseUrl;
+
+if(process.env.SERVER === 'prod') {
+    baseUrl = 'http://www.google.com';
+} else {
+    baseUrl = 'http://www.webdriveruniversity.com';
+}
+
 exports.config = {
     
     //
@@ -77,7 +85,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'www.webdriveruniversity.com',
+    baseUrl: baseUrl,
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
