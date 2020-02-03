c2da929e529a644f5f590b7b66ec05fb27e5af38
Juan David Sánchez <juancho85@gmail.com>
2020-02-03 21:08:23 +0100

    Custom commands and javascript execute command

--- tests/contactUs.js
+++ tests/contactUs.js
@@ -1,32 +1,44 @@
+const request = require('sync-request');
+
+browser.addCommand("submitDataViaContactUsForm", function (firstName, lastName, emailAddress, comments) {
+  if(firstName) {
+    browser.setValue("[name='first_name']", firstName);
+  }
+  if(lastName) {
+    browser.setValue("[name='last_name']", lastName);
+  }
+  if(emailAddress) {
+    browser.setValue("[name='email']", emailAddress);
+  }
+  if(comments) {
+    browser.setValue("[name='message']", comments);
+  }
+  browser.click("[type='submit']");
+});
+
 beforeEach(function() {
   browser.url('/Contact-Us/contactus.html');
 });
 
 describe('Test Contact Us form WebdriverUni', function() {
+  const res = request('GET', 'https://jsonplaceholder.typicode.com/comments?postId=1');
+  const contactUsDetails = JSON.parse(res.getBody().toString('utf8'));
   let contactUsPageTitle;
   beforeEach(function() {
     contactUsPageTitle = browser.getTitle();
     console.log(`navigated to contact Us page. Title is ${contactUsPageTitle}`);
   });
 
-  it('Should be able to submit a successful submission via contact us form', function(done) {
-    browser.setValue("[name='first_name']", 'Paco');
-    browser.setValue("[name='last_name']", 'Palotes');
-    browser.setValue("[name='email']", 'paco@test.com');
-    browser.setValue("[name='message']", 'Hola qué tal?');
-    browser.click("[type='submit']");
-    browser.waitUntil(
-        function() {
-          return browser.getTitle() !== contactUsPageTitle
-        },
-        2000,
-        "No navigation to another page after contact us form submission"
-    );
-    const successfulContactConfirmation = browser.isExisting('#contact_reply h1');
-    expect(successfulContactConfirmation, 'Successful submission message does not exist').to.be.true;
-    const successFullSubmissionText = browser.getText('#contact_reply h1');
-    expect(successFullSubmissionText).to.equal('Thank You for your Message!');
+  contactUsDetails.forEach(function (contactUsDetail) {
+    it('Should be able to submit a successful submission via contact us form', function(done) {
+      browser.submitDataViaContactUsForm('Paco', 'Palotes', contactUsDetail.email, contactUsDetail.body);
+      const successfulContactConfirmation = browser.isExisting('#contact_reply h1');
+      expect(successfulContactConfirmation, 'Successful submission message does not exist').to.be.true;
+      const successFullSubmissionText = browser.getText('#contact_reply h1');
+      expect(successFullSubmissionText).to.equal('Thank You for your Message!');
+    });
   });
+
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', function(done) {
     browser.setValue("[name='first_name']", 'Pepe');
