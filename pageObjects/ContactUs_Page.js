312f9d4143f7a515f0933a573430edda696ee09b
Juan David Sánchez <juancho85@gmail.com>
2020-02-03 22:27:54 +0100

    Page Object Model - phase 3 finished

--- pageObjects/ContactUs_Page.js
+++ pageObjects/ContactUs_Page.js
@@ -6,6 +6,56 @@ class ContactUs_Page {
   get submitButton() { return $("[type='submit']");}
   get successfulSubmissionHeader() { return $("#contact_reply h1");}
   get unsuccessfulSubmissionHeader() { return $("body");}
+
+  setFirstName(firstName) {
+    return this.firstName.setValue(firstName);
+  }
+
+  setLastName(lastName) {
+    return this.lastName.setValue(lastName);
+  }
+
+  setEmailAddress(emailAddress) {
+    return this.emailAddress.setValue(emailAddress);
+  }
+
+  setComments(comments) {
+    return this.comments.setValue(comments);
+  }
+
+  clickSubmitButton() {
+    return this.submitButton.click();
+  }
+
+  submitFormWithData(firstName, lastName, emailAddress, comments) {
+    if(firstName) {
+      this.firstName.setValue(firstName);
+    }
+    if(lastName) {
+      this.lastName.setValue(lastName);
+    }
+    if(emailAddress) {
+      this.emailAddress.setValue(emailAddress);
+    }
+    if(comments) {
+      this.comments.setValue(comments);
+    }
+    this.submitButton.click();
+    this.confirmSuccessfulSubmission();
+  }
+
+  confirmSuccessfulSubmission() {
+    const validateSubmissionHeader = browser.waitUntil(() => {
+      return this.successfulSubmissionHeader.getText() === 'Thank You for your Message!'
+    }, 3000);
+    expect(validateSubmissionHeader, 'Successful submission message does not exist').to.be.true;
+  }
+
+  confirmUnsuccessfulSubmission() {
+    expect(this.unsuccessfulSubmissionHeader.getText()).to.include('Error: all fields are required');
+  }
 }
+
+
 
 module.exports = new ContactUs_Page();
--- tests/contactUs.js
+++ tests/contactUs.js
@@ -9,69 +9,34 @@ describe('Test Contact Us form WebdriverUni', function() {
   const res = request('GET', 'https://jsonplaceholder.typicode.com/comments?postId=1');
   const contactUsDetails = JSON.parse(res.getBody().toString('utf8'));
 
-  function setFirstName(firstName) {
-    return ContactUs_Page.firstName.setValue(firstName);
-  }
-
-  function setLastName(lastName) {
-    return ContactUs_Page.lastName.setValue(lastName);
-  }
-
-  function setEmailAddress(emailAddress) {
-    return ContactUs_Page.emailAddress.setValue(emailAddress);
-  }
-
-  function setComments(comments) {
-    return ContactUs_Page.comments.setValue(comments);
-  }
-
-  function clickSubmitButton() {
-    return ContactUs_Page.submitButton.click();
-  }
-
-  function confirmSuccessfulSubmissions() {
-    const validateSubmissionHeader = browser.waitUntil(function () {
-      return ContactUs_Page.successfulSubmissionHeader.getText() === 'Thank You for your Message!'
-    }, 3000);
-    expect(validateSubmissionHeader, 'Successful submission message does not exist').to.be.true;
-  }
-
-  function confirmUnsuccessfulSubmissions() {
-    expect(ContactUs_Page.unsuccessfulSubmissionHeader.getText()).to.include('Error: all fields are required');
-  }
-
   contactUsDetails.forEach(function (contactUsDetail) {
     it('Should be able to submit a successful submission via contact us form', function(done) {
-      setFirstName('Paco');
-      setLastName('Palotes');
-      setEmailAddress(contactUsDetail.email);
-      setComments(contactUsDetail.body);
-      clickSubmitButton();
-      confirmSuccessfulSubmissions();
+      ContactUs_Page.submitFormWithData('Paco', 'Palotes', contactUsDetail.email, contactUsDetail.body);
     });
   });
 
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', function(done) {
-    setFirstName('Paco');
-    setLastName('Palotes');
-    setEmailAddress('paco@palotes.com');
-    clickSubmitButton();
-    confirmUnsuccessfulSubmissions();
+    ContactUs_Page.setFirstName('Paco');
+    ContactUs_Page.setLastName('Palotes');
+    ContactUs_Page.setEmailAddress('paco@palotes.com');
+    ContactUs_Page.clickSubmitButton();
+    ContactUs_Page.confirmUnsuccessfulSubmission();
   });
 
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', function(done) {
-    setFirstName('Paola');
-    setEmailAddress('paola@perales.com');
-    clickSubmitButton();
-    confirmUnsuccessfulSubmissions();
+    ContactUs_Page.setFirstName('Paola');
+    ContactUs_Page.setEmailAddress('paola@perales.com');
+    ContactUs_Page.setComments('A comment');
+    ContactUs_Page.clickSubmitButton();
+    ContactUs_Page.confirmUnsuccessfulSubmission();
   });
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', function(done) {
-    setFirstName('Paco');
-    setLastName('Palotes');
-    clickSubmitButton();
-    confirmUnsuccessfulSubmissions();
+    ContactUs_Page.setFirstName('Paco');
+    ContactUs_Page.setLastName('Palotes');
+    ContactUs_Page.clickSubmitButton();
+    ContactUs_Page.confirmUnsuccessfulSubmission();
   });
 });
